9d0f0147a6623d4b5297aa522e5aa8bee4b2d5d1
Gabriel-Fachini <gabriel_fachini@usp.br>
2021-03-05 12:48:24 -0300

    Integração com o javaScript
    Foram adicionados as funcionalidades dos seletores e barra de pesquisa.

--- scripts/back.js
+++ scripts/back.js
@@ -28,7 +28,7 @@ let materias = [
         nome_en_us: 'Linear Algebra',
         creditos_aula: 2,
         creditos_trabalho: 1,
-        carga_horaria_total: 90,
+        carga_horaria_total: 80,
         tipo: 'semestral',
         objetivos: 'Aprofundar conceitos de geometria analítica',
     }
@@ -36,6 +36,188 @@ let materias = [
 
 //Variável que seleciona a div que irá mostrar as matérias
 let mainContainer = document.getElementById('container-materias');
+
+//Variável que seleciona o select do filtro e ordenação
+let selectFiltro = document.getElementById('select-filtro');
+let selectOrdenacao = document.getElementById('select-ordenacao');
+let barraPesquisa = document.getElementById('barra-pesquisa');
+
+//Variáveis auxiliares
+let filtroSelecionado;
+let ordenacaoSelecionada;
+let stringPesquisaAtual = '';
+
+//Adicionando event listener na barra de pesquisa
+barraPesquisa.addEventListener('input', (event) => {
+    stringPesquisaAtual = event.target.value;
+    updateRenderizacao();
+});
+
+//Adicionando o event listener no select de fitro
+selectFiltro.addEventListener('change', (event) => {
+    let filtro = event.target.value;
+    if(filtro == "Selecione") {
+        filtroSelecionado = undefined;
+    }
+    if(filtro == "Carga horária 90h"){
+        filtroSelecionado = filtraMateriaCargaHoraria90;
+    }
+    if(filtro == "Creditos aula 4"){
+        filtroSelecionado = filtraMateriaCreditos4;
+    }
+    if(filtro == "Creditos aula 2"){
+        filtroSelecionado = filtraMateriaCreditos2;
+    }
+    updateRenderizacao();
+});
+
+//Adicionando o event listener no select de ordenação
+selectOrdenacao.addEventListener('change', (event) => {
+    let ordenacao = event.target.value;
+    if(ordenacao == "Selecione"){
+        ordenacaoSelecionada = undefined;
+    }
+    if(ordenacao == "Alfabética Crescente"){
+        ordenacaoSelecionada = bubbleSort;
+    }
+    if(ordenacao =="Alfabética Decrescente"){
+        ordenacaoSelecionada = bubbleSortInvertido;
+    }
+    updateRenderizacao();
+});
+
+//Função que vai gerenciar os filtros, ordenações e pesquisa aplicando-os independentemente
+function updateRenderizacao () {
+    limparMaterias();
+    let materiasFiltradas;
+
+    // Filtro
+    if (filtroSelecionado !== undefined) {
+        materiasFiltradas = filtroSelecionado();
+    } else {
+        materiasFiltradas = materias;
+    }
+
+    //Pesquisa
+    let materiasPesquisadas;
+    if (stringPesquisaAtual === '') {
+        materiasPesquisadas = materiasFiltradas;
+    } else {
+        materiasPesquisadas = materiasFiltradas.filter(
+            function (materia) {
+                // Se for pra manter a matéria na array, returna true
+                // Se for pra remover a matéria da array, returna false
+                return materia.nome_pt_br.toLowerCase().includes(stringPesquisaAtual.toLowerCase().trim());
+            }
+        );
+    }
+
+    let materiasOrdenadas;
+
+    // Ordenacao
+    if (ordenacaoSelecionada !== undefined) {
+        materiasOrdenadas = ordenacaoSelecionada(materiasPesquisadas);
+    } else {
+        materiasOrdenadas = materiasPesquisadas;
+    }
+
+    renderizarMaterias(materiasOrdenadas);
+}
+
+//Bubble sort para ordenação Decrescente das matérias
+function bubbleSortInvertido(materias){
+    let x, y;
+    let buffer = [];
+    let materiasOrdenadas = [];
+    for(let i = 0; i < materias.length; i++){
+        materiasOrdenadas[i] = materias[i];
+    }
+
+    for(x=0; x< materias.length; x++){
+        for(y=x+1; y < materias.length; y++){
+            if(materiasOrdenadas[x].nome_pt_br.localeCompare(materias[y].nome_pt_br) < 0){
+                buffer[0] = materiasOrdenadas[x];
+                materiasOrdenadas[x]=materiasOrdenadas[y];
+                materiasOrdenadas[y]= buffer[0];
+            }
+        }
+    }
+
+    return materiasOrdenadas;
+}
+
+// materias.sort(
+//     function (a, b) {
+//         // return um numero maior que zero se A tiver que aparecer antes de B
+//         // return um numero menor que zero se A tiver que aparecer depois de B
+//         // return um numero  zero se A for igual a B
+//     }
+// );
+
+//Bubble sort para ordenação das matérias
+function bubbleSort(materias){
+    let x, y;
+    let buffer = [];
+    let materiasOrdenadas = [];
+    for(let i = 0; i < materias.length; i++){
+        materiasOrdenadas[i] = materias[i];
+    }
+
+    for(x=0; x< materias.length; x++){
+        for(y=x+1; y < materias.length; y++){
+            if(materiasOrdenadas[x].nome_pt_br.localeCompare(materias[y].nome_pt_br) > 0){
+                buffer[0] = materiasOrdenadas[x];
+                materiasOrdenadas[x]=materiasOrdenadas[y];
+                materiasOrdenadas[y]= buffer[0];
+            }
+        }
+    }
+
+    return materiasOrdenadas;
+}
+
+
+//Função que filtra as matérias que tem 90 de carga horária
+function filtraMateriaCargaHoraria90 (){
+    let materiasFiltered = [];
+
+    for(let i=0; i < materias.length; i++){
+        let materia = materias[i];
+        if(materia.carga_horaria_total === 90){
+            materiasFiltered.push(materia);
+        }
+    }
+
+    return materiasFiltered
+}
+
+//Função que filtra as matérias por creditos 4
+function filtraMateriaCreditos4 (){
+    let materiasFiltered = [];
+
+    for(let i=0; i < materias.length; i++){
+        let materia = materias[i];
+        if(materia.creditos_aula === 4){
+            materiasFiltered.push(materia);
+        }
+    }
+
+    return materiasFiltered
+}
+
+//Função que filtra as matérias por creditos 2
+function filtraMateriaCreditos2 (){
+    let materiasFiltered = [];
+
+    for(let i=0; i < materias.length; i++){
+        let materia = materias[i];
+        if(materia.creditos_aula === 2){
+            materiasFiltered.push(materia);
+        }
+    }
+
+    return materiasFiltered
+}
 
 //Função que cria uma div e coloca as informações dentro dessa div de uma determinada matéria
 function criarContainerDeUmaMateria(materia){
@@ -75,7 +257,7 @@ function renderizarMaterias (materias){
         });
         renderizarElementoDeMateria(containerMateria);
     }
- }
+}
 
 //Chamada da função que renderiza as matérias
 renderizarMaterias(materias);
